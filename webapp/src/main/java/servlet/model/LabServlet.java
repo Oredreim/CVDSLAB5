8b33d5d5b2b354800b9f2e7771ffeadfbc2b70c4
jsebasg <juan.garcia-h@mail.escuelaing.edu.co>
2021-03-04 11:40:15 -0500

    parte 3

--- webapp/src/main/java/servlet/model/LabServlet.java
+++ webapp/src/main/java/servlet/model/LabServlet.java
@@ -44,5 +44,33 @@ public class LabServlet extends HttpServlet{
 
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    	Writer responseWriter = resp.getWriter();
+     	Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+    	String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "";
+    try
+        {
+        	int intId = Integer.parseInt(id);
+            Todo todo = Service.getTodo(intId); 
+            ArrayList<Todo> todoList = new ArrayList<Todo>();
+            todoList.add(todo);
+            responseWriter.write(Service.todosToHTMLTable(todoList));
+            resp.setStatus(HttpServletResponse.SC_OK);
+            responseWriter.flush();
+        }
+        catch (MalformedURLException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch (FileNotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        catch (Exception e){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+        }
+    }
 }
 
